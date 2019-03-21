a67de5834b75e26c40862387cb0300399634308b
William Bradshaw <wbradshaw4@my.bcit.ca>
2019-03-21 11:18:52 -0700

    Created files for models

---
+++ app/Models/sizes.php
@@ -0,0 +1,19 @@
+class SizesModel extends \CodeIgniter\Model
+{
+
+        protected $table      = 'dwarfs';
+        protected $primaryKey = 'id';
+
+		protected $returnType = 'array';
+        protected $useSoftDeletes = true;
+
+        protected $allowedFields = ['name', 'role'];
+
+        protected $useTimestamps = false;
+
+        protected $validationRules    = [];
+        protected $validationMessages = [];
+        protected $skipValidation     = false;
+
+		
+}
