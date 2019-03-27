7525f0996efc219b8ec43f621b50b7ff4c0d9620
William Bradshaw <wbradshaw4@my.bcit.ca>
2019-03-27 14:23:21 -0700

    Added DwarfView

---
+++ app/Views/DwarfView.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <title>COMP 4711 - Lab 06</title>
+</head>
+<body>
+    <h1>William Bradshaw</h1>
+    <h1>Jeffrey Stephen</h1>
+	
+	 <h3>Dwarves</h3>
+
+        <ul>
+        <?php foreach ($data as $item):?>
+
+                <li><?= $item ?></li>
+
+        <?php endforeach;?>
+        </ul>
+	
+</body>
+</html>
